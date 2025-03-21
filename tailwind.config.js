a8fe1d255433c3123c49c75262370e040a5924b0
rahmatjon <tom98john@gmail.com>
2025-03-21 18:12:39 +0500

    avion div1

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/ @type {import('tailwindcss').Config} */
+export default {
+  darkMode:"class",
+   content: ["./src//*.{html,js}"],
+   theme: {
+     extend: {},
+screens: {
+      'lg': '640px',  // Desktops
+  },
+   },
+   plugins: [],
+ }
+
